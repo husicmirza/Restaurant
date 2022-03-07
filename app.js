cac05a82ca5c9ce807085f2c166f6cd68e1c9544
husicmirza <mirza.huc@gmail.com>
2022-03-07 21:26:57 +0100

    update style

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ function article(item) {
   <div class="item-info">
     <header>
       <h4>${item.title}</h4>
-      <h4 class="price">${item.price}</h4>
+      <h4 class="price">${item.price}$</h4>
     </header>
     <p class="item-text">${item.desc}</p>
   </div>
